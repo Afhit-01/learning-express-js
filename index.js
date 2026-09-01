8117d30fa1d5d1744aaea98155c3126ef2c5df1a
Fatihu AbdulGaniyu <abdulghaniyyfaatih2@gmail.com>
2026-09-01 15:12:49 +0100

    added a new endpoint

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const app = express();
 
 const port = 3000;
 
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World</h1>");
+});
+
+app.get("/about", (req, res) => {
+  res.send("<h1>About Me</h1>");
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
